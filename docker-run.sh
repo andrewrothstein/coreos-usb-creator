79df798d0a9e1614cd6c25fb35205f3f12954a3b
Andrew Rothstein <andrew.rothstein@gmail.com>
2016-11-27 00:49:16 -0500

    allow autologin. make console useful.

--- docker-run.sh
+++ docker-run.sh
@@ -7,6 +7,7 @@ docker run --rm -it \
        -v $HOME/.ssh:$HOME/.ssh \
        -v $(pwd)/out:/out \
        coreos-usb-creator \
+       --autologin \
        --coreoschannel stable \
        --cloudconfigurl https://raw.githubusercontent.com/andrewrothstein/drewfus-org-cloud-inits/master/drewfus.org/usbkey.yml \
        -o /out/usbkey.drewfus.org
